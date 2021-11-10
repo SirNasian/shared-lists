3caa15aabab11389fe86b745e5555c63ac0cc32b
Nathan Iszlaub <NathanIszlaub@gmail.com>
2021-11-10 23:17:17 +0800

    Add stubbed pages `Overview` and `Editor`

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { Editor, Overview } from "./pages";
+
+type PAGE = "overview" | "editor";
+
 const App = () => {
-	return null;
+	const [page, setPage] = React.useState<PAGE>("overview");
+	switch (page) {
+		case "overview": return <Overview />;
+		case "editor":   return <Editor />;
+	}
 };
 
 ReactDOM.render(<App />, document.querySelector("#react"));
